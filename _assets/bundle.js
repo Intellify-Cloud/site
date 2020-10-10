b29b753ead30de62a79fd578f315411a57202f5a
Eben <me@ebenroux.co.za>
2020-10-10 16:37:54 +0200

    - initial commit

---
+++ _assets/bundle.js
@@ -0,0 +1,8 @@
+import 'expose-loader?exposes[]=$&exposes[]=jQuery!jquery';
+import 'expose-loader?exposes[]=Popper!popper.js';
+import 'jquery.easing';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './site.scss';
+import './site';
